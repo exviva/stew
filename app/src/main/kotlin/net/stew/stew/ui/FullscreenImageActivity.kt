44b9ef431e7728d94d33977ffd9ad66231795582
Olek Janiszewski <olek.janiszewski@gmail.com>
2020-06-18 15:43:46 +0200

    Support only Android 6+

--- app/src/main/kotlin/net/stew/stew/ui/FullscreenImageActivity.kt
+++ app/src/main/kotlin/net/stew/stew/ui/FullscreenImageActivity.kt
@@ -4,7 +4,6 @@ import android.app.Activity
 import android.app.ActivityOptions
 import android.content.Intent
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.view.View
 import com.facebook.drawee.backends.pipeline.Fresco
@@ -15,17 +14,14 @@ class FullscreenImageActivity : Activity() {
 
     companion object {
         fun start(activity: Activity, uri: Uri, view: View) {
-            val intent = Intent(activity, FullscreenImageActivity::class.java)
-            intent.putExtra("uri", uri)
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                val width = 0.5 * view.width
-                val height = 0.5 * view.height
-                val bundle = ActivityOptions.makeScaleUpAnimation(view, width.toInt(), height.toInt(), 0, 0).toBundle()
-                activity.startActivity(intent, bundle)
-            } else {
-                activity.startActivity(intent)
+            val intent = Intent(activity, FullscreenImageActivity::class.java).apply {
+                putExtra("uri", uri)
             }
+            val width = 0.5 * view.width
+            val height = 0.5 * view.height
+            val bundle = ActivityOptions.makeScaleUpAnimation(view, width.toInt(), height.toInt(), 0, 0).toBundle()
+
+            activity.startActivity(intent, bundle)
         }
     }
 
@@ -34,13 +30,14 @@ class FullscreenImageActivity : Activity() {
 
         setContentView(R.layout.activity_fullscreen_image)
 
-        val uri: Uri = intent.getParcelableExtra("uri")!!
-        val controller = Fresco.newDraweeControllerBuilder().
-            setUri(uri).
-            setAutoPlayAnimations(true).
-            build()
-        imageView.controller = controller
-        imageView.setOnClickListener { finish() }
+        imageView.apply {
+            controller = Fresco
+                    .newDraweeControllerBuilder()
+                    .setUri(intent.getParcelableExtra<Uri>("uri"))
+                    .setAutoPlayAnimations(true)
+                    .build()
+            setOnClickListener { finishAfterTransition() }
+        }
     }
 
 }
